65403b2b8802d9f97a26f087b8a4e0b798e5251f
Alexander Purr <purr@ub.uni-leipzig.de>
2026-08-21 15:12:44 +0200

    feat(refactor): simplifies ParamsTrait
    * by call parent getBackendParameters() and just add needed collapse & expand parameters
    * by dropping full copy of outdated getFilterSettings() from VuFind 4.1 -> using parent by inheritance from now

--- src/Search/Solr/ParamsTrait.php
+++ src/Search/Solr/ParamsTrait.php
@@ -40,55 +40,6 @@ use VuFindSearch\ParamBag;
  */
 trait ParamsTrait
 {
-    /**
-     * Return the current filters as an array of strings ['field:filter']
-     *
-     * @return array $filterQuery
-     */
-    public function getFilterSettings()
-    {
-        // Define Filter Query
-        $filterQuery = [];
-        $orFilters = [];
-        $filterList = array_merge(
-            $this->getHiddenFilters(),
-            $this->filterList
-        );
-        foreach ($filterList as $field => $filter) {
-            if ($orFacet = (substr($field, 0, 1) === '~')) {
-                $field = substr($field, 1);
-            }
-            if ($filter === '') {
-                continue;
-            }
-            foreach ($filter as $value) {
-                // Special case -- complex filter, that should be taken as-is:
-                if ($field == '#') {
-                    $q = $value;
-                } elseif (
-                    substr($value, -1) === '*'
-                    || preg_match('/\[[^\]]+\s+TO\s+[^\]]+\]/', $value)
-                ) {
-                    // Special case -- allow trailing wildcards and ranges
-                    $q = $field . ':' . $value;
-                } else {
-                    $q = $field . ':"' . addcslashes($value, '"\\') . '"';
-                }
-                if ($orFacet) {
-                    $orFilters[$field] ??= [];
-                    $orFilters[$field][] = $q;
-                } else {
-                    $filterQuery[] = $q;
-                }
-            }
-        }
-        foreach ($orFilters as $field => $parts) {
-            $filterQuery[] = '{!tag=' . $field . '_filter}' . $field
-                . ':(' . implode(' OR ', $parts) . ')';
-        }
-        return $filterQuery;
-    }
-
     // Construct collapse parameters
     public function constructingCollapseParams()
     {
@@ -127,7 +78,8 @@ trait ParamsTrait
      */
     public function getBackendParameters()
     {
-        $backendParams = new ParamBag();
+        $backendParams = parent::getBackendParameters();
+
         // restore grouping settings from cookie
         $this->collapseExpandConfig->restoreFromCookie();
 
@@ -143,79 +95,6 @@ trait ParamsTrait
             foreach ($this->constructingExpandParams() as $key => $value) {
                 $backendParams->add($key, $value);
             }
-        }
-        // search those shards that answer, accept partial results
-        $backendParams->add('shards.tolerant', 'true');
-
-        // maximum search time in ms
-        // $backendParams->add('timeAllowed', '4000');
-
-        // defaultOperator=AND was removed in schema.xml
-        $backendParams->add('q.op', 'AND');
-
-        // increase performance for facet queries
-        $backendParams->add('facet.threads', '4');
-
-        // Spellcheck
-        $backendParams->set(
-            'spellcheck',
-            $this->getOptions()->spellcheckEnabled() ? 'true' : 'false'
-        );
-
-        // Facets
-        $facets = $this->getFacetSettings();
-        if (!empty($facets)) {
-            $backendParams->add('facet', 'true');
-
-            foreach ($facets as $key => $value) {
-                // prefix keys with "facet" unless they already have a "f." prefix:
-                $fullKey = substr($key, 0, 2) === 'f.' ? $key : "facet.$key";
-                $backendParams->add($fullKey, $value);
-            }
-            $backendParams->add('facet.mincount', 1);
-        }
-
-        // Filters
-        $filters = $this->getFilterSettings();
-        foreach ($filters as $filter) {
-            $backendParams->add('fq', $filter);
-        }
-
-        // Shards
-        $allShards = $this->getOptions()->getShards();
-        $shards = $this->getSelectedShards();
-        if (empty($shards)) {
-            $shards = array_keys($allShards);
-        }
-
-        // If we have selected shards, we need to format them:
-        if (!empty($shards)) {
-            $selectedShards = [];
-            foreach ($shards as $current) {
-                $selectedShards[$current] = $allShards[$current];
-            }
-            $shards = $selectedShards;
-            $backendParams->add('shards', implode(',', $selectedShards));
-        }
-
-        // Sort
-        $sort = $this->getSort();
-        if ($sort) {
-            // If we have an empty search with relevance sort, see if there is
-            // an override configured:
-            if (
-                $sort == 'relevance' && $this->getQuery()->getAllTerms() == ''
-                && ($relOv = $this->getOptions()->getEmptySearchRelevanceOverride())
-            ) {
-                $sort = $relOv;
-            }
-            $backendParams->add('sort', $this->normalizeSort($sort));
-        }
-
-        // Pivot facets for visual results
-
-        if ($pf = $this->getPivotFacets()) {
-            $backendParams->add('facet.pivot', $pf);
         }
 
         return $backendParams;
